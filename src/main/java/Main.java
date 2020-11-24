226daad12c2b8f9fa61b74bea126596d43054fff
lukaszkosminski <dnb8@o2.pl>
2020-11-24 15:09:20 +0100

    ex a - c

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,85 @@
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] input = {45, -3, -2, -1, 0, 1, 2, 3, 4, 5, -3, 10, 11, -500, 1000};
-        exA(input);
+        int[] inputInt = {45, 0, -3, -2, -1, 0, 1, 2, 3, 4, 5, -3, 10, 11, -500, 1000};
+        String[] inputString = {"Andrzej", "Romuald", "Lukasz", "Michal", "Jaroslaw"};
+        //  exA(inputInt);
+        //  exB(inputInt);
+        //  exC(inputString);
+        exD();
 
     }
 
     public static void exA(int[] input) {
-        int [] wynik = new int[2];
-        for (int i = 0; i <input.length; i++){
-            if (input[i]<0) {
-                wynik[0]++;
-            }
-            else{
-                wynik[1] += input[i];
+        int[] output = new int[2];
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] < 0) {
+                output[0]++;
+            } else {
+                output[1] += input[i];
             }
         }
-        System.out.println(wynik[0]);
-        System.out.println(wynik[1]);
+        System.out.println(output[0]);
+        System.out.println(output[1]);
     }
-    public static void exB(int[]input){
-        int[]wynik = new int[5];
 
-        for (int i = 0; i < input.length;i++){
+    public static void exB(int[] input) {
+        int[] output = new int[5];
 
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] < 0) {
+                output[0]++;
+                output[3] += input[i];
+            }
+            if (input[i] >= 0) {
+                output[1]++;
+                output[4] += input[i];
+
+            }
+            if (input[i] == 0) {
+                output[2]++;
+            }
+        }
+        System.out.println("number of negative numbers = " + output[0]);
+        System.out.println("number of positive numbers = " + output[1]);
+        System.out.println("number of zeros = " + output[2]);
+        System.out.println("sum of negative numbers = " + output[3]);
+        System.out.println("sum of positive numbers = " + output[4]);
+    }
+
+    public static void exC(String[] stringArray) {
+        System.out.println("podaj litere");
+        Scanner scanner = new Scanner(System.in);
+        String letter = scanner.nextLine().toLowerCase();
+        LinkedHashMap<String, Boolean> maps = new LinkedHashMap<>();
+
+        for (int i = 0; i < stringArray.length; i++) {
+            if (stringArray[i].toLowerCase().contains(letter)) {
+                maps.put(stringArray[i], true);
+                System.out.println(stringArray[i] + ", " + true);
+            } else if (!stringArray[i].toLowerCase().contains(letter)) {
+                maps.put(stringArray[i], false);
+                System.out.println(stringArray[i] + ", " + false);
+            }
         }
 
+    }public static void exD(){
+        Instant now = Instant.now();
+        System.out.println(now);
+
+        LocalDateTime now2 = LocalDateTime.now();
+
+        System.out.println(now2.plus(60, ChronoUnit.SECONDS));
+        System.out.println();
 
     }
 }
+
